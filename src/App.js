a223afa445cf37d74bc1efae2eeaa29b9a9addc3
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-31 12:45:55 +0100

    included search component to app and styled search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <>
       <NavBar />
+      <Header />
     </>
   );
 }
