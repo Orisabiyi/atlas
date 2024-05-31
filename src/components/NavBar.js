614898f87d0bd2303fa800b6db1c8a1b7102f03a
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-31 11:34:16 +0100

    created the navbar, included an asset folder for the project

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <h1>AtlasApp</h1>
+      <div className="navbar__mode">
+        <img src="asset/moon.svg" alt="moon icon" />
+        <h2>Dark Mode</h2>
+      </div>
+    </nav>
+  );
+}
