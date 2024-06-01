13e2a5ddb30784e396b83f2b98dc616e24352f24
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-01 10:07:23 +0100

    called the countries api, included it in the UI and handled a crashing error with the program

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,11 +1,33 @@
-export default function CountryCard() {
-  const getCountries = async function () {
-    const res = await fetch("https://restcountries.com/v3.1/all");
-    const data = res.json();
-    console.log(data);
-  };
+export default function CountryCard({ data }) {
+  // data?.map((item) => console.log(item));
+  console.log(data);
 
-  getCountries();
-
-  return <div className="header__card"></div>;
+  return (
+    <>
+      {data?.map((item) => {
+        return (
+          <div className="card">
+            <div className="card__img">
+              <img src={item.flags.png} alt={item.flags.alt} />
+            </div>
+            <div className="card__info">
+              <h2>{item.name.official}</h2>
+              <p>
+                <span>Population:</span>
+                {item.population}
+              </p>
+              <p>
+                <span>Region:</span>
+                {item.population}
+              </p>
+              <p>
+                <span>Capital:</span>
+                {item.capital}
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
+import CountryCard from "./CountryCard";
 import Filter from "./Filter";
 import Search from "./Search";
 
 export default function Header() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const getCountries = async function () {
+    try {
+      const res = await fetch("https://restcountries.com/v3.1/all");
+      const data = await res.json();
+      setData(data);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => getCountries, []);
+
   return (
     <header className="header">
       <div className="header__search">
         <Search />
         <Filter />
+      </div>
+
+      <div className="header__card">
+        {data ? <CountryCard data={data} /> : ""}
       </div>
     </header>
   );
