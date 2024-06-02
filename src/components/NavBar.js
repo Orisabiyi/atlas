ad0f0ecc9102278513f46ac43b8ed7623881d944
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-02 02:42:56 +0100

    added a state to handle dark/light mode

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 export default function NavBar() {
+  const [isDark, setIsDark] = useState(false);
+
+  function handleClick() {
+    setIsDark((mode) => !mode);
+  }
+
   return (
     <nav className="navbar">
       <h1>AtlasApp</h1>
-      <div className="navbar__mode">
+      <div role="button" className="navbar__mode" onClick={handleClick}>
         <img src="asset/moon.svg" alt="moon icon" />
         <h2>Dark Mode</h2>
       </div>
