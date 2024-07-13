bbdf97caf949ed4baa7d7c495de3a5518e7e790c
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-13 04:12:37 +0100

    refactor countrycard component

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,27 +1,10 @@
+import CountryItem from "./CountryItem";
+
 export default function CountryCard({ data }) {
   return (
     <>
       {data?.map((item) => (
-        <div className="card" key={item?.name?.official}>
-          <div className="card__img">
-            <img src={item?.flags?.png} alt={item?.name?.official} />
-          </div>
-          <div className="card__info">
-            <h2>{item?.name?.official}</h2>
-            <p>
-              <span>Population: </span>
-              {Intl.NumberFormat().format(item?.population)}
-            </p>
-            <p>
-              <span>Region: </span>
-              {item?.region}
-            </p>
-            <p>
-              <span>Capital: </span>
-              {item?.capital}
-            </p>
-          </div>
-        </div>
+        <CountryItem item={item} />
       ))}
     </>
   );
