af570d71278c57f180f0c8427d56a7c63a672200
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-13 04:14:25 +0100

    include a link for reload in the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function NavBar({ isDark = "false", handleClick, children }) {
   return (
     <nav className={`navbar ${isDark ? "navbar-dark" : ""}`}>
-      <h1>{children}</h1>
+      <Link to="/">
+        <h1>{children}</h1>
+      </Link>
       <div role="button" className="navbar__mode" onClick={handleClick}>
         <img
           src={isDark ? "asset/moon.svg" : "asset/moon-dark.png"}
