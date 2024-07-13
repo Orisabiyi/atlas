6c72231264f96b5cd7f93c7491224e3c7154a921
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-13 04:11:24 +0100

    include the jsx for countryitem and props

---
+++ src/components/CountryItem.jsx
@@ -0,0 +1,26 @@
+function CountryItem({ item }) {
+  return (
+    <div className="card" key={item?.name?.official}>
+      <div className="card__img">
+        <img src={item?.flags?.png} alt={item?.name?.official} />
+      </div>
+      <div className="card__info">
+        <h2>{item?.name?.official}</h2>
+        <p>
+          <span>Population: </span>
+          {Intl.NumberFormat().format(item?.population)}
+        </p>
+        <p>
+          <span>Region: </span>
+          {item?.region}
+        </p>
+        <p>
+          <span>Capital: </span>
+          {item?.capital}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CountryItem;
