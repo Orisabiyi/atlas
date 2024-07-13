b9df474779f067d76e40d6488efdc7c069875c18
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-13 03:50:54 +0100

    made the filter component controlled

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,6 @@
 export default function Filter({ region, onSetRegion }) {
   return (
-    <select name="filter" value={region} onChange={onSetRegion}>
+    <select name="filter" value={region} onChange={(e) => onSetRegion(e)}>
       <option value="africa">Africa</option>
       <option value="america">America</option>
       <option value="asia">Asia</option>
