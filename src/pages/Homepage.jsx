c7f6eeba0ce8b25505b4234f324ba9e64b30a625
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-13 04:00:59 +0100

    create a homepage file and include the necessary required

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+
+function Homepage() {
+  const [isDark, setIsDark] = useState(false);
+
+  function handleClick() {
+    setIsDark((mode) => !mode);
+  }
+
+  return (
+    <>
+      <NavBar isDark={isDark} handleClick={handleClick} />
+      <Header isDark={isDark} />
+    </>
+  );
+}
+
+export default Homepage;
