e677b80827ac6b875816bdcf3ea9c6cc03c519aa
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-17 16:28:50 +0100

    Refactor App component to add dark mode functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
+  function handleClick() {
+    setIsDark((mode) => !mode);
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}>
-          <Route index element={<Header />} />
+        <Route
+          path="/"
+          element={<Homepage isDark={isDark} handleClick={handleClick} />}
+        >
+          <Route index element={<Header isDark={isDark} />} />
         </Route>
       </Routes>
     </BrowserRouter>
