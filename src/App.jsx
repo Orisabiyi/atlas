2859f908cfe8dc7009828f20e96a5e4586fbf0f2
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-29 19:30:00 +0100

    chore: Add CountryContext provider to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Header from "./components/Header";
 import CountryDetail from "./components/CountryDetail";
+import { CountryContext } from "./contexts/CountryContext";
 
 function App() {
   const [isDark, setIsDark] = useState(false);
@@ -12,17 +13,19 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<Homepage isDark={isDark} handleClick={handleClick} />}
-        >
-          <Route index element={<Header isDark={isDark} />} />
-          <Route path="/:country" element={<CountryDetail />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CountryContext>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Homepage isDark={isDark} handleClick={handleClick} />}
+          >
+            <Route index element={<Header isDark={isDark} />} />
+            <Route path="/:country" element={<CountryDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CountryContext>
   );
 }
 
