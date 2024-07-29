de56c280967df3ff7213ace9addecfa72b8282cf
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-29 19:27:56 +0100

    feat: Add lazy loading for images in CountryContext

--- src/contexts/CountryContext.jsx
+++ src/contexts/CountryContext.jsx
@@ -1,9 +1,103 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const countriesContext = createContext();
 
 function CountryContext({ children }) {
-  return <countriesContext.Provider>{children}</countriesContext.Provider>;
+  const [search, setSearch] = useState("all");
+  const [cache, setCache] = useState({});
+  const [error, setError] = useState("");
+  const [region, setRegion] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+
+      const fetchCountries = async function () {
+        if (search && cache[search]) {
+          setCountries(cache[search]);
+          // setRegion("");
+          setError("");
+        }
+
+        if (search && !cache[search]) {
+          setIsLoading(true);
+          try {
+            const response = await fetch(
+              `https://restcountries.com/v3.1/${
+                search !== "all" ? `name/${search}` : "all"
+              }`,
+              { signal: controller.signal }
+            );
+
+            if (!response.ok) throw new Error("Country not found");
+
+            const data = await response.json();
+
+            setCountries(data);
+            setCache((prevCache) => ({ ...prevCache, [search]: data }));
+            // setRegion("");
+            setError("");
+          } catch (error) {
+            if (error.name === "AbortError") return;
+            setError(error.message);
+          }
+          setIsLoading(false);
+        }
+      };
+
+      fetchCountries();
+
+      // cleanup
+      return function () {
+        controller.abort();
+      };
+    },
+    [search, cache]
+  );
+
+  useEffect(
+    function () {
+      const fetchCountriesByRegion = async function () {
+        if (region && cache[region]) {
+          setCountries(cache[region]);
+          setSearch("all");
+          setError("");
+        }
+
+        if (region && !cache[region]) {
+          try {
+            setIsLoading(true);
+            const response =
+              await fetch(`https://restcountries.com/v3.1/region/${region}
+          `);
+            const data = await response.json();
+
+            setCountries(data);
+            setCache((prevCache) => ({ ...prevCache, [region]: data }));
+            setSearch("all");
+            setError("");
+          } catch (error) {
+            setError(error.message);
+          } finally {
+            setIsLoading(false);
+          }
+        }
+      };
+
+      fetchCountriesByRegion();
+    },
+    [region, cache]
+  );
+
+  return (
+    <countriesContext.Provider
+      value={{ setSearch, setRegion, error, region, countries, isLoading }}
+    >
+      {children}
+    </countriesContext.Provider>
+  );
 }
 
 function useCountryContext() {
