f108a759cbdaa4023516990e047b395062083322
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-29 19:43:58 +0100

    feat: Implement getCurrentCountry function in CountryDetail component

--- src/components/CountryDetail.jsx
+++ src/components/CountryDetail.jsx
@@ -1,8 +1,18 @@
 import { useParams } from "react-router-dom";
+import { useCountryContext } from "../contexts/CountryContext";
+import { useEffect } from "react";
 
 function CountryDetail() {
   const { country } = useParams();
-  console.log(country);
+  const { getCurrentCountry } = useCountryContext();
+
+  useEffect(
+    function () {
+      getCurrentCountry(country);
+    },
+    [country, getCurrentCountry]
+  );
+
   return <section></section>;
 }
 
