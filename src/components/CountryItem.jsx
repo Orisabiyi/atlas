bda1a70e7269e1fbbf22582d5dd46e83d446733e
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-11 13:49:18 +0100

    chore: replace the useNavigate with a link

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,33 +1,29 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function CountryItem({ item }) {
-  const navigate = useNavigate();
-
   return (
-    <div
-      className="card hover:cursor-pointer"
-      key={item?.name?.official}
-      onClick={() => navigate(item.name.official)}
-    >
-      <div className="card__img">
-        <img src={item?.flags?.png} alt={item?.name?.official} />
+    <Link to={`/${item.name.official}`}>
+      <div className="card hover:cursor-pointer" key={item?.name?.official}>
+        <div className="card__img">
+          <img src={item?.flags?.png} alt={item?.name?.official} />
+        </div>
+        <div className="card__info">
+          <h2>{item?.name?.official}</h2>
+          <p>
+            <span>Population: </span>
+            {Intl.NumberFormat().format(item?.population)}
+          </p>
+          <p>
+            <span>Region: </span>
+            {item?.region}
+          </p>
+          <p>
+            <span>Capital: </span>
+            {item?.capital}
+          </p>
+        </div>
       </div>
-      <div className="card__info">
-        <h2>{item?.name?.official}</h2>
-        <p>
-          <span>Population: </span>
-          {Intl.NumberFormat().format(item?.population)}
-        </p>
-        <p>
-          <span>Region: </span>
-          {item?.region}
-        </p>
-        <p>
-          <span>Capital: </span>
-          {item?.capital}
-        </p>
-      </div>
-    </div>
+    </Link>
   );
 }
 
