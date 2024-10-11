088f5d93611e25a18836f04a3a5701b90a1e41c2
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-11 11:58:37 +0100

    feat: update the title name of the project

--- src/components/CountryDetail.jsx
+++ src/components/CountryDetail.jsx
@@ -1,19 +1,26 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useCountryContext } from "../contexts/CountryContext";
 import { useEffect } from "react";
 
 function CountryDetail() {
   const { country } = useParams();
   const { getCurrentCountry } = useCountryContext();
+  const navigate = useNavigate("");
 
   useEffect(
     function () {
       getCurrentCountry(country);
     },
-    [country, getCurrentCountry]
+    [country]
   );
 
-  return <section></section>;
+  return (
+    <section>
+      <button onClick={() => navigate(-1)}>
+        <span>&larr;</span> <span>Back</span>
+      </button>
+    </section>
+  );
 }
 
 export default CountryDetail;
